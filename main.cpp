8b4d2c7ef5ac50361fcf5d17dce22e00a618b207
corneleaa <kornelijadam0909@gmail.com>
2025-09-14 18:25:38 +0300

    Create main.cpp
    ivedziau visas reikiamas bibliotekas tolimesniam darbui

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::setw;
+using std::left;
+using std::right;
+using std::fixed;
+using std::setprecision;
