5a9ea762256717dc22d1ab5c6578e3674236e277
corneleaa <kornelijadam0909@gmail.com>
2025-09-18 01:04:12 +0300

    Update main.cpp
    cia teko viska lygiuotis ir kopijuotis is naujo nes programa del eiliskumo nepaisymo pridejus failu nuskaityma pradejo labai labai kreivai veikti. Veike, bet ne taip kaip norejau

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include <fstream>
 #include <sstream>
 
-
 using std::cout;
 using std::cin;
 using std::endl;
@@ -30,10 +29,11 @@ struct Studentas {
     string vardas;
     string pavarde;
     vector<int> pazymiai;
-    int egzaminas;
-    double galutinisVid;
-    double galutinisMed;
+    int egzaminas{};
+    double galutinisVid{};
+    double galutinisMed{};
 };
+
 // vidurkio skaiciavimo funkcija
 double average(const vector<int>& v) {
     if (v.empty()) return 0.0;
@@ -52,23 +52,13 @@ double median(vector<int> v) {
     else
         return v[n/2];
 }
-int pasirinkimoMeniu() {
-    int p;
-    cout << "Pasirinkite veiksma:\n";
-    cout << "1 - Ivesti duomenis ranka\n";
-    cout << "2 - Sugeneruoti atsitiktinai\n";
-    cout << "3- Nuskaityti is failo\n"
-    cout << "Jusu pasirinkimas: ";
-    cin >> p;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    return p;
-}
+
 Studentas generuotiStudenta() {
     Studentas s;
     s.vardas = "Vardas" + std::to_string(std::rand() % 100);
     s.pavarde = "Pavarde" + std::to_string(std::rand() % 100);
     int nd = 1 + std::rand() % 5;
-    for (int i = 0; i < nd; i++) s.pazymiai.push_back(1 + std::rand() % 10);
+    for (int i = 0; i < nd; i++)
         s.pazymiai.push_back(1 + std::rand() % 10);
 
     s.egzaminas   = 1 + std::rand() % 10;
@@ -76,6 +66,7 @@ Studentas generuotiStudenta() {
     s.galutinisMed = median(s.pazymiai) * 0.4 + s.egzaminas * 0.6;
     return s;
 }
+
 vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
     vector<Studentas> grupe;
     ifstream in(failoVardas);
@@ -85,7 +76,7 @@ vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
     }
 
     string eilute;
-    getline(in, eilute);
+    getline(in, eilute); // praleidžiam antraštę
 
     while (getline(in, eilute)) {
         if (eilute.empty()) continue;
@@ -108,50 +99,72 @@ vector<Studentas> nuskaitytiIsFailo(const string& failoVardas) {
     }
     return grupe;
 }
+
 int main() {
     std::srand(std::time(nullptr));
-    cout << "Studentu informacine sistema\n"; // ties cia stop
-    
-    int veiksmas = pasirinkimoMeniu();
-    int n;
-    cout << "Kiek studentu norite ivesti? ";
-    cin >> n;
-    
+    cout << "Studentu informacine sistema\n";
+
+    cout << "Pasirinkite veiksma:\n";
+    cout << "1 - Ivesti duomenis ranka\n";
+    cout << "2 - Sugeneruoti atsitiktinai\n";
+    cout << "3 - Nuskaityti is failo\n";
+    cout << "Jusu pasirinkimas: ";
+
+    int veiksmas;
+    cin >> veiksmas;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
     vector<Studentas> grupe;
-    
+    int n = 0;
+
     if (veiksmas == 1) {
+        cout << "Kiek studentu norite ivesti? ";
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
         for (int i = 0; i < n; i++) {
             Studentas s;
             cout << "Iveskite varda: ";
             cin >> s.vardas;
             cout << "Iveskite pavarde: ";
             cin >> s.pavarde;
-            cout << "Iveskite namu darbu pazymius (Enter uzbaigs irasyma):\n";
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Iveskite namu darbu pazymius (Enter uzbaigs irasyma):\n";
             while (true) {
-                string eilute;
-                getline(cin, eilute);
-                if (eilute.empty()) break;
-                s.pazymiai.push_back(std::stoi(eilute));
+                string eil;
+                getline(cin, eil);
+                if (eil.empty()) break;
+                s.pazymiai.push_back(std::stoi(eil));
             }
+
             cout << "Egzamino rezultatas: ";
             cin >> s.egzaminas;
-            
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
             s.galutinisVid = average(s.pazymiai) * 0.4 + s.egzaminas * 0.6;
             s.galutinisMed = median(s.pazymiai) * 0.4 + s.egzaminas * 0.6;
             grupe.push_back(s);
         }
-    } else {
-        for (int i = 0; i < n; i++)
-            grupe.push_back(generuotiStudenta());
+
+    } else if (veiksmas == 2) {
+        cout << "Kiek studentu sugeneruoti? ";
+        cin >> n;
+        for (int i = 0; i < n; i++) grupe.push_back(generuotiStudenta());
+
+    } else if (veiksmas == 3) {
+        string path = "/Users/kornelijadambrauskaite/Desktop/projektas/projektas/projektas/studentai10000.txt";
+        grupe = nuskaitytiIsFailo(path);
     }
+
     int pasirinkimas;
-    cout << "Pasirinkite isvedimo buda:\n";
+    cout << "\nPasirinkite isvedimo buda:\n";
     cout << "1 - Tik pagal vidurki\n";
     cout << "2 - Tik pagal mediana\n";
     cout << "3 - Abu kartu\n";
     cout << "Jusu pasirinkimas: ";
     cin >> pasirinkimas;
+
     std::sort(grupe.begin(), grupe.end(),
               [](const Studentas &a, const Studentas &b) {
                   return a.vardas < b.vardas;
@@ -170,7 +183,7 @@ int main() {
              << setw(20) << right << "Galutinis (Med.)";
 
     cout << endl;
-    cout << "-----------------------------------------------------------------------" << endl;
+    cout << string(70, '-') << endl;
 
     for (auto &st : grupe) {
         cout << setw(15) << left << st.vardas
@@ -189,4 +202,5 @@ int main() {
 
     return 0;
 }
+
 
